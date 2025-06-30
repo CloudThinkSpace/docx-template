7918d82598b9d7fb89749c6c72b4db2004e82d2e
飘香随云 <wozaifeiyang0@163.com>
2025-06-30 10:55:52 +0800

    修复bug：如果类型不是图片，获取链接的扩展名

--- src/request.rs
+++ src/request.rs
@@ -1,6 +1,6 @@
 use crate::error::DocxError;
-use reqwest::Client;
 use crate::image::get_extension;
+use reqwest::Client;
 
 /// 获取图片数据   
 /// @param client 请求客户端  
@@ -19,16 +19,17 @@ pub async fn request_image_data(
     // 获取请求头
     let headers = response.headers().clone();
     // 先读取头信息
-    let mut content_type = headers
+    let content_type = headers
         .get(reqwest::header::CONTENT_TYPE)
-        .map(|h| h.to_str().unwrap_or("").to_string())
-        .unwrap_or("".to_string());
+        .map(|h| h.to_str().unwrap_or(""))
+        .unwrap_or("");
     // 获取内容类型以验证是否为图片
-    if !content_type.starts_with("image/") {
-        content_type = get_extension(url)?.to_string();
-    }
+    let extension = if !content_type.starts_with("image/") {
+        get_extension(url)?
+    } else {
+        &content_type[6..]
+    };
     // 读取字节
     let image_data = response.bytes().await?.to_vec();
-    let extension = &content_type[6..];
     Ok((image_data, extension.to_string()))
 }
